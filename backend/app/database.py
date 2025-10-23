04b484c04016560bf569348390868a293e71f3c0
knayo <dezxny@gmail.com>
2025-10-23 11:26:21 +0900

    database 일부 수정

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "semi")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "zz123")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "himedia")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
